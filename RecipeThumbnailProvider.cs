02133412095855878d7b7a79dbe72a87e089aee8
ozan <ozan@kulahci.com>
2012-10-07 22:13:52 +0000

    Very First Commit

---
+++ RecipeThumbnailProvider.cs
@@ -0,0 +1,90 @@
+﻿/********************************** Module Header **********************************\
+Module Name:  RecipeThumbnailProvider.cs
+Project:      CSShellExtThumbnailHandler
+Copyright (c) Microsoft Corporation.
+
+The RecipeThumbnailProvider.cs file defines a thumbnail handler for .recipe files.
+
+This source is subject to the Microsoft Public License.
+See http://www.microsoft.com/opensource/licenses.mspx#Ms-PL.
+All other rights reserved.
+
+THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, EITHER 
+EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED WARRANTIES OF 
+MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
+\***********************************************************************************/
+
+#region Using directives
+using System;
+using System.IO;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using Microsoft.WindowsAPICodePack.ShellExtensions;
+using CSharpTricks.Photoshop;
+using System.Windows.Forms;
+#endregion
+
+
+namespace CSShellExtThumbnailHandler
+{
+    [ClassInterface(ClassInterfaceType.None)]
+    [Guid("8B95D7DF-2DB4-4A8D-8BE0-DBC986D385BD"), ComVisible(true)]
+    [ThumbnailProvider("OzzPdfThumbnailProvider", ".psd", DisableProcessIsolation=true)]
+    public class OzzPdfThumbnailProvider : ThumbnailProvider, IThumbnailFromFile
+    {
+        #region IThumbnailFromStream Members
+
+        public Bitmap ConstructBitmap(Stream stream, int sideSize)
+        {
+            return null;
+            try
+            {
+               // MessageBox.Show("1. adim");   stream.Position = 0;
+             
+                CPSD psd = new CPSD();
+                psd.Load(stream);
+               // MessageBox.Show("3. adim");
+                //FileStream tmpStrm = new FileStream(@"c:\sil.psd", FileMode.Create);
+                
+                //stream.CopyTo(tmpStrm);
+                Bitmap psdBitmap = Image.FromHbitmap(psd.GetHBitmap());
+                Size size = new Size();
+
+                if (psdBitmap.Width > psdBitmap.Height)
+                {
+                    size.Height = psdBitmap.Height * sideSize / psdBitmap.Width;
+                    size.Width = sideSize;
+                }
+                else
+                {
+                    size.Width = psdBitmap.Width * sideSize / psdBitmap.Height;
+                    size.Height = sideSize;
+                }
+               // ((Bitmap)ThumbCreator.ThumbCreator.ResizeImage(psdBitmap, size)).Save(@"c:\sil.bmp");
+
+                return ThumbCreator.ThumbCreator.ResizeImage(psdBitmap, size) as Bitmap;
+                //using (var Thumbnail = new Bitmap(cx, cx))
+                //{
+                //  using (var G = Graphics.FromImage(Thumbnail))
+                //  {
+                //    G.Clear(Color.Red);
+                //  }
+                //  hBitmap = Thumbnail.GetHbitmap();
+                //}
+            }
+            catch { } // A dirty cop-out.
+            return Resource1.Dummy;
+        }
+
+        #endregion
+
+        public Bitmap ConstructBitmap(FileInfo info, int sideSize)
+        {
+            CPSD psd = new CPSD();
+            psd.Load(info.FullName);
+                //MessageBox.Show(psd.IsThumbnailIncluded().ToString());
+                return Bitmap.FromStream(new MemoryStream(psd.m_ThumbNail.Data)) as Bitmap;
+            
+        }
+    }
+}
---
+++ TestApp/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using CSShellExtThumbnailHandler;
+using System.IO;
+using CSharpTricks.Photoshop;
+
+namespace TestApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OzzPdfThumbnailProvider tmp = new OzzPdfThumbnailProvider();
+            FileStream strm = new FileStream(@"C:\Users\okulahci\Desktop\Resim\EzgiFeritCep\CepAlbumSablonu2.psd", FileMode.Open);
+            pictureBox1.Image = tmp.ConstructBitmap(strm, 96);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            CPSD psd = new CPSD();
+            psd.Load(@"C:\Users\okulahci\Desktop\Resim\05.psd");
+            //MessageBox.Show(psd.IsThumbnailIncluded().ToString());
+            pictureBox1.Image = Bitmap.FromStream(new MemoryStream(psd.m_ThumbNail.Data));
+
+        }
+    }
+}
