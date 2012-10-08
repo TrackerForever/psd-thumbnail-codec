df9c8ac6ffb0148bbb3e9b13a8b99515cad5e4f4
ozan@kulahci.com <ozan@kulahci.com@f8486736-dc9a-6ba2-8ce2-ef9040bd6e15>
2012-10-08 15:28:50 +0000

    Wix Setup added

---
+++ trunk/CustomAction1/CustomAction.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text;
+using Microsoft.Deployment.WindowsInstaller;
+using CSShellExtThumbnailHandler;
+
+namespace InstallAction
+{
+    public class CustomActions
+    {
+        [CustomAction]
+        public static ActionResult Install(Session session)
+        {
+            session.Log("Begin Install");
+            //ThumbCreator.ThumbCreator a = new ThumbCreator.ThumbCreator(); 
+            // Call RegistrationServices.RegisterAssembly to register the classes in 
+            // the current managed assembly to enable creation from COM.
+            RegistrationServices regService = new RegistrationServices();
+            regService.RegisterAssembly(
+                
+                typeof(ThumbCreator.ThumbCreator).Assembly,
+                AssemblyRegistrationFlags.SetCodeBase);
+            return ActionResult.Success;
+        }
+
+        [CustomAction]
+        public static ActionResult UnInstall(Session session)
+        {
+            session.Log("Begin UnInstall");
+            // Call RegistrationServices.UnregisterAssembly to unregister the classes 
+            // in the current managed assembly.
+            RegistrationServices regService = new RegistrationServices();
+            regService.UnregisterAssembly(typeof(ThumbCreator.ThumbCreator).Assembly);
+            return ActionResult.Success;
+        }
+    }
+}
